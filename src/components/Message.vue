e1891e27a70dd193df4b1b384eaf2364e34b7425
laola1technik <laola1technik@users.noreply.github.com>
2018-03-23 18:04:48 +0100

    Fills Message.

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -2,30 +2,53 @@
   <div class="text-box-highlights">
     <div class="left">
       <div class="sportart">
-        <span class="sprite-actions-small basketball"></span>
+          <span v-if="hasSportsType"
+                class="sprite-actions-small" :class="sportsTypeClass">
+          </span>
       </div>
       <div class="zeit">
-        <span>20:43 Uhr</span>
+        <span>{{message.time}}</span>
       </div>
     </div>
     <div class="right">
       <div class="text">
-        <p class="title">Basketball - Finale Herren</p>
+        <p class="title">{{message.title}}</p>
         <span>
-                                    Die letzte Entscheidung bei diesen Spielen fällt im Basketball. Im Finale der Herren stehen sich die USA und Serbien gegenüber.
-                                </span>
+          {{message.text}}
+        </span>
       </div>
     </div>
-    <div class="medaille gold"></div>
+    <div v-if="hasType"
+         :class="typeClass">
+    </div>
   </div>
 </template>
 
 <script>
     export default {
-        name: "message"
+        props: [ 'message' ],
+        methods: {},
+        computed: {
+            hasType() {
+                return this.message.type !== '';
+            },
+            hasSportsType() {
+                return this.message.sportsType !== '';
+            },
+            sportsTypeClass() {
+                return this.message.sportsType.toLowerCase();
+            },
+            typeClass() {
+                const typeToClass = {
+                    'goldMedal': 'medaille gold'
+                };
+
+                if (typeof typeToClass[this.message.type] !== 'undefined') {
+                    return typeToClass[this.message.type];
+                }
+
+                return '';
+            }
+        }
     }
 </script>
-
-<style scoped>
-
-</style>
