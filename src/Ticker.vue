e0d166730c73636a5054eae18c770a053e338090
laola1technik <laola1technik@users.noreply.github.com>
2018-03-23 14:22:30 +0100

    Integrates olympia.html into Ticker Component.

---
+++ src/Ticker.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+  <header>
+    <div class="header_logocontainer config_json_aktualisieren">
+      <div class="header_logo">
+        <p class="header_logo_bewerb">Live-Ticker Olympische Spiele</p>
+        <a target="_blank" href="http://liveticker.laola1.at"><img class="header_logo_img" src="assets/img/laola1_liveticker_small.png"></a>
+        <div class="header_logo_aktualisieren" >aktualisieren
+          <div class="progressbar">
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="header_spcontainer2 config_json_nav" id="liveticker-menu"><!-- Container for Game Navigation -->
+
+      <div class="header_sp2"><!-- Game Navigation-->
+        <div class="header_sp_nextbuttonlinks2 icon- inactive">&laquo;</div>
+
+        <ul class="slider1">
+          <li class="header_sp_spielbutton2 slide active">
+            <p class="header_sp_spielbutton_nav2"><span>Live</span></p>
+          </li>
+          <li class="header_sp_spielbutton2 slide">
+            <p class="header_sp_spielbutton_nav2"><span>Programm</span></p>
+          </li>
+        </ul>
+        <div class="header_sp_nextbuttonrechts2 icon- inactive">&raquo;</div>
+      </div>
+    </div>
+
+  </header>
+  <div class="main_container">
+    <div class="main">
+      <div class="stage">
+        <!-- LIVE START -->
+        <div class="full-width" id="liveticker-tickertext">
+          <h2>LIVE</h2>
+          <div class="filter">
+            <section class="select-wrapper filter-sportart single-filter">
+              <label class="custom-select">
+                Filtern nach Sportart:
+                <select class="sportsTypeFilter">
+                  <option selected="selected" value="0">Alle Sportarten</option>
+                  <option value="1">Badminton</option>
+                  <option value="2">Baseball</option>
+                </select>
+              </label>
+            </section>
+          </div>
+          <div class="content_container">
+            <div class="text-box-highlights">
+              <div class="left">
+                <div class="sportart">
+                </div>
+                <div class="zeit">
+                  <span>20:46 Uhr</span>
+                </div>
+              </div>
+              <div class="right">
+                <div class="text">
+                  <p class="title">Das waren die Olympischen Spiele in Rio</p>
+                  <span>
+                                    Das US-Basketball-Team setzt den Schlusspunkt bei den Spielen 2016! Spannende 16 Wettkampf-Tage liegen hinter uns - 306 Mal ging es um Gold, Silber und Bronze. Aber auch abseits der Medaillen gab es schöne Momente im Zeichen der fünf Ringe. Wir hoffen, ihr hatte Spaß mit unserem Live-Ticker.
+                                </span>
+                </div>
+              </div>
+              <div class=""></div>
+            </div>
+
+            <div class="text-box-highlights">
+              <div class="left">
+                <div class="sportart">
+                  <span class="sprite-actions-small basketball"></span>
+                </div>
+                <div class="zeit">
+                  <span>20:43 Uhr</span>
+                </div>
+              </div>
+              <div class="right">
+                <div class="text">
+                  <p class="title">Basketball - Finale Herren</p>
+                  <span>
+                                    Die letzte Entscheidung bei diesen Spielen fällt im Basketball. Im Finale der Herren stehen sich die USA und Serbien gegenüber.
+                                </span>
+                </div>
+              </div>
+              <div class="medaille gold"></div>
+            </div>
+
+            <div class="text-box-highlights placeholder">
+              <div class="left">
+                <div class="sportart"></div>
+                <div class="zeit"><span></span></div>
+              </div>
+              <div class="">
+                <div class="text">
+                  <p class="title"></p>
+                  <span>Es wurden keine passenden Einträge gefunden</span>
+                </div>
+              </div>
+              <div class=""></div>
+            </div>
+            <!--- text-box-highlights ende -->
+          </div>
+        </div>
+
+        <div class="paging-container" id="liveticker-tickertext-paging">
+          <a href="javascript:void(0)" class="boxarrow right">mehr anzeigen <span><img src="assets/img/arrow-right.png" alt="next" /></span></a>
+        </div>
+        <!-- LIVE ENDE -->
+        <!-- FOOTER START -->
+        <div class="footer">
+          <span>produced by</span><a href="http://www.laola1.at" target="_blank"><img src="assets/img/l1logosmall.png"></a>
+        </div>
+        <!-- FOOTER ENDE-->
+      </div> <!-- STAGE  ENDE -->
+    </div> <!-- MAIN  ENDE -->
+  </div> <!-- MAIN_CONTAINER ENDE -->
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "ticker"
+    }
+</script>
+
+<style scoped>
+
+</style>
