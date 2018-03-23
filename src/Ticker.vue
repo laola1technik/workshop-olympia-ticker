7265f1a959057fb92b3c213707c76fad749bea20
laola1technik <laola1technik@users.noreply.github.com>
2018-03-23 15:57:41 +0100

    Extracts Header component.

--- src/Ticker.vue
+++ src/Ticker.vue
@@ -1,36 +1,6 @@
 <template>
   <div>
-  <header>
-    <div class="header_logocontainer config_json_aktualisieren">
-      <div class="header_logo">
-        <p class="header_logo_bewerb">Live-Ticker Olympische Spiele</p>
-        <a target="_blank" href="http://liveticker.laola1.at"><img class="header_logo_img" src="./assets/laola1_liveticker_small.png"></a>
-        <div class="header_logo_aktualisieren" >aktualisieren
-          <div class="progressbar">
-            <div></div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="header_spcontainer2 config_json_nav" id="liveticker-menu"><!-- Container for Game Navigation -->
-
-      <div class="header_sp2"><!-- Game Navigation-->
-        <div class="header_sp_nextbuttonlinks2 icon- inactive">&laquo;</div>
-
-        <ul class="slider1">
-          <li class="header_sp_spielbutton2 slide active">
-            <p class="header_sp_spielbutton_nav2"><span>Live</span></p>
-          </li>
-          <li class="header_sp_spielbutton2 slide">
-            <p class="header_sp_spielbutton_nav2"><span>Programm</span></p>
-          </li>
-        </ul>
-        <div class="header_sp_nextbuttonrechts2 icon- inactive">&raquo;</div>
-      </div>
-    </div>
-
-  </header>
+  <header-template/>
   <div class="main_container">
     <div class="main">
       <div class="stage">
@@ -122,8 +92,11 @@
 </template>
 
 <script>
+    import HeaderTemplate from './components/Header.vue';
+
     export default {
-        name: "ticker"
+        name: "ticker",
+        components: { HeaderTemplate }
     }
 </script>
 
